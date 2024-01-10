162bf6aee996913716c27db1c27c58424de66562
Masood169 <155927504+Masood169@users.noreply.github.com>
2024-01-11 04:22:35 +0500

    timeout

--- assets/custom.js
+++ assets/custom.js
@@ -5,6 +5,7 @@
 // Insert any custom theme js here...
 
 // Wait for the iframe to load
+setTimeout(function() {
 $('div#WebPixelsManagerSandboxContainer + script + iframe').on('load', function() {
     // Set a timeout to run your script after 2 seconds
     setTimeout(function() {
@@ -12,3 +13,4 @@ $('div#WebPixelsManagerSandboxContainer + script + iframe').on('load', function(
         // Add your other code here if needed
     }, 1000);
 });
+}, 1000);
