2dfd88b48888342d6058d67f8e7c035b59e33c28
Masood169 <155927504+Masood169@users.noreply.github.com>
2024-01-11 01:35:11 +0500

    remove attr

--- assets/custom.js
+++ assets/custom.js
@@ -4,14 +4,15 @@
 
 // Insert any custom theme js here...
 $('ul.slick-dots li').removeAttr('role', 'presentation');
+$('ul.slick-dots li').removeAttr('aria-controls');
 $('#slick-slide00, #slick-slide01').click(function() {
     var clickedLi = $(this);
 
     // Set a timeout to run the effect after 3 seconds
     setTimeout(function() {
         // Remove 'role' attribute from both li elements
-        $('#slick-slide00, #slick-slide01').removeAttr('role', 'presentation');
-
+        clickedLi.removeAttr('role', 'presentation');
+        clickedLi.removeAttr('aria-controls');
         // Add your other code here if needed
     }, 3000);
 });
