9fc8a594d773d03f7990b9c07300c13e1613f3c7
Masood169 <155927504+Masood169@users.noreply.github.com>
2024-01-11 04:36:56 +0500

    aria-selected=""

--- assets/custom.js
+++ assets/custom.js
@@ -14,3 +14,14 @@ $('div#WebPixelsManagerSandboxContainer + script + iframe').on('load', function(
     }, 1000);
 });
 }, 1000);
+
+$(".product-single__swatch__item input:nth-child(1)").attr("aria-selected", "true");
+$(document).ready(function() {
+    $('.product-single__swatch input[type="radio"]').click(function() {
+      // Set aria-select="false" for all inputs
+      $('.product-single__swatch input[type="radio"]').attr('aria-select', 'false');
+
+      // Set aria-select="true" for the clicked input
+      $(this).attr('aria-select', 'true');
+    });
+  });
