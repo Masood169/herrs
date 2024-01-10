ed7f646f1b7c4e31a73499f6c3b8006609efc676
Masood169 <155927504+Masood169@users.noreply.github.com>
2024-01-11 01:31:30 +0500

    test

--- assets/custom.js
+++ assets/custom.js
@@ -4,12 +4,15 @@
 
 // Insert any custom theme js here...
 $('ul.slick-dots li').removeAttr('role', 'presentation');
-$('ul.slick-dots li').click(function() {
+$('#slick-slide00, #slick-slide01').click(function() {
     var clickedLi = $(this);
-    // Set a timeout to run the script after 3 seconds
+
+    // Set a timeout to run the effect after 3 seconds
     setTimeout(function() {
-        clickedLi.removeAttr('role', 'presentation');
-        // Add your other code here to be executed after 3 seconds
+        // Remove 'role' attribute from both li elements
+        $('#slick-slide00, #slick-slide01').removeAttr('role', 'presentation');
+
+        // Add your other code here if needed
     }, 3000);
 });
 
