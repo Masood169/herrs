c33bfd601242f79dd7b39d0abd4cf67d72b39d71
Masood169 <155927504+Masood169@users.noreply.github.com>
2024-01-11 04:19:22 +0500

    add title

--- assets/custom.js
+++ assets/custom.js
@@ -4,3 +4,11 @@
 
 // Insert any custom theme js here...
 
+// Wait for the iframe to load
+$('div#WebPixelsManagerSandboxContainer + script + iframe').on('load', function() {
+    // Set a timeout to run your script after 2 seconds
+    setTimeout(function() {
+        $('div#WebPixelsManagerSandboxContainer + script + iframe').attr('title', 'test');
+        // Add your other code here if needed
+    }, 2000);
+});
