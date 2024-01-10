d0fb6fcbafe6f4747732d60dba9726a640ee21bc
Masood169 <155927504+Masood169@users.noreply.github.com>
2024-01-11 03:24:04 +0500

    fixes for voice-over

--- assets/custom.js
+++ assets/custom.js
@@ -4,25 +4,3 @@
 
 // Insert any custom theme js here...
 
-$(document).ready(function() {
-    // Check if .slick-dots class is present on the page
-    if ($('#slick-slide00, #slick-slide01').length > 0) {
-        setTimeout(function() {
-            $('#slick-slide00, #slick-slide01').removeAttr('role', 'presentation');
-            $('#slick-slide00, #slick-slide01').removeAttr('aria-controls');
-                        // Add your other code here if needed
-        }, 1000);
-    }
-});
-
-$('#slick-slide00, #slick-slide01').click(function() {
-
-    // Set a timeout to run the effect after 3 seconds
-    setTimeout(function() {
-        // Remove 'role' attribute from both li elements
-        $('#slick-slide00, #slick-slide01').removeAttr('role', 'presentation');
-        $('#slick-slide00, #slick-slide01').removeAttr('aria-controls');
-        // Add your other code here if needed
-    }, 3000);
-});
-
