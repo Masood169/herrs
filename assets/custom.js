e526b36270ea24a9cc5a064938f17a1339e65c51
Masood169 <155927504+Masood169@users.noreply.github.com>
2024-01-11 01:28:32 +0500

    set time out

--- assets/custom.js
+++ assets/custom.js
@@ -4,7 +4,12 @@
 
 // Insert any custom theme js here...
 $('ul.slick-dots li').removeAttr('role', 'presentation');
-$('ul.slick-dots li').click(function(){
-    $(this).removeAttr('role', 'presentation');
+$('ul.slick-dots li').click(function() {
+    var clickedLi = $(this);
+    // Set a timeout to run the script after 3 seconds
+    setTimeout(function() {
+        clickedLi.removeAttr('role', 'presentation');
+        // Add your other code here to be executed after 3 seconds
+    }, 3000);
 });
 
